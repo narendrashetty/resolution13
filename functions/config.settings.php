bacaf1d3810fdcf04f63040a5692e3d08712bff7
Swaroop SM <swaroop.striker@gmail.com>
2013-08-26 21:55:21 +0530

    added default settings file

---
+++ functions/config.settings.php
@@ -0,0 +1,13 @@
+# Create a new file called config.php in functions/ and dump and change the following settings
+
+<?php
+
+	$host = ""; # MySQL hostname; default: localhost
+	$user = ""; # MySQL username; default: root
+	$pwd = "";  # MySQL password
+	$db = "";		# Set Your dbname; preferably: resolution13
+
+	mysql_connect($host, $user, $pwd);
+	mysql_select_db($db);
+
+?>
